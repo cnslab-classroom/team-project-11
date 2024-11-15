2e09f9df997b915121cdd7f5deb1def87c99289f
LeeDohoun <dohoon9945@gmail.com>
2024-11-16 00:59:01 +0900

    feat: TextToPromptManager 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
 import controller.ProjectController;
+import model.TextToPromptManager;
 public class App {
     public static void main(String[] args) {
-        ProjectController projectController = new ProjectController();
-        projectController.run();
+        //ProjectController projectController = new ProjectController();
+        //projectController.run();
+        TextToPromptManager textToPromptManager = new TextToPromptManager();
+        //영어일기 내용 입력
+        String inputText = "The girl is reading a book under the maple tree.";
+        String prompt = textToPromptManager.convertToPrompt(inputText);
+        System.out.println("Prompt: " + prompt);
     }
 }
