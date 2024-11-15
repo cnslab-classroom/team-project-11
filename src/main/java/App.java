035bf6fa4e43befe9269d464155d224b9e9512fa
shin <79005737+Shinhisu@users.noreply.github.com>
2024-11-15 18:22:50 +0900

    feat: 기본 스켈레톤 코드 구현

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import controller.ProjectController;
+public class App {
+    public static void main(String[] args) {
+        ProjectController projectController = new ProjectController();
+        projectController.run();
+    }
+}
