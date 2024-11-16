a245496ae8766499e23d58fb1f412b0a54449bfc
star-candy <el0710@naver.com>
2024-11-16 11:03:58 +0900

    test: PromptToImageManagerTest 구현
    이미지 정상 출력 확인

--- src/test/java/model/PromptToImageManagerTest.java
+++ src/test/java/model/PromptToImageManagerTest.java
@@ -1,8 +1,18 @@
 package model;
 
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
+
+
 
 public class PromptToImageManagerTest {
 
-}
+    @Test
+    public void testImage() throws IOException {
+        String prompt = "a cat wearing a hat";
+        PromptToImageManager manager = new PromptToImageManager(prompt);
+        manager.requestHuggingface();
+        manager.downloadImage();
+    }
 
+}
