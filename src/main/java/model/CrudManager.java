5bea152f4a288c299fb7083effbff90da3bbe906
are012 <qjqqmfql19@naver.com>
2024-11-17 13:04:50 +0900

    feat: CrudManager, FileIoManager, BoardService 구현

--- src/main/java/model/CrudManager.java
+++ src/main/java/model/CrudManager.java
@@ -3,13 +3,49 @@ package model;
 import dto.BoardDto;
 import java.util.Vector;
 
-//BoardDto 구조체들을 벡터로 관리--> 아직 구현 안함
-
 public class CrudManager {
- 
-    // requestCreate(BoardDto post) 게시글 객체 저장
-    // requestRead(int index) 특정 인덱스에 해당하는 게시글 객체 반환
-    // requestUpdate(BoardDto post) 기존 게시글 수정
-    // requestDelete(int index) 게시글 삭제 
-    // requestAll() 전체게시글 조회
+    private final Vector<BoardDto> boardDb;
+
+    public CrudManager() {
+        this.boardDb = new Vector<>();
+    }
+
+    // 게시글 생성
+    public void requestCreate(BoardDto post) {
+        boardDb.add(post);
+        System.out.println("게시글 생성 완료: " + post.getTitle());
+    }
+
+    // 특정 인덱스 게시글 읽기
+    public BoardDto requestRead(int index) {
+        if (index >= 0 && index < boardDb.size()) {
+            return boardDb.get(index);
+        }
+        throw new IndexOutOfBoundsException("잘못된 인덱스입니다: " + index);
+    }
+
+    // 게시글 수정
+    public void requestUpdate(int index, BoardDto post) {
+        if (index >= 0 && index < boardDb.size()) {
+            boardDb.set(index, post);
+            System.out.println("게시글 수정 완료 (인덱스: " + index + ")");
+        } else {
+            throw new IndexOutOfBoundsException("잘못된 인덱스입니다: " + index);
+        }
+    }
+
+    // 게시글 삭제
+    public void requestDelete(int index) {
+        if (index >= 0 && index < boardDb.size()) {
+            BoardDto removedPost = boardDb.remove(index);
+            System.out.println("게시글 삭제 완료: " + removedPost.getTitle());
+        } else {
+            throw new IndexOutOfBoundsException("잘못된 인덱스입니다: " + index);
+        }
+    }
+
+    // 전체 게시글 반환
+    public Vector<BoardDto> requestAll() {
+        return boardDb;
+    }
 }
